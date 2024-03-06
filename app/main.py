c6911877631e90e647962c7f041963cbe81dbea5
Alexander Todorov <41835213+tdrv90@users.noreply.github.com>
2024-03-07 00:07:13 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ from src.carsanalyzer import CarAnalyzer
 
 analyzer = CarAnalyzer()
 
-analyzer.load_data_from_json("D:\\dev\\cars\\cars.json")
-analyzer.get_unique_car_count()
-analyzer.get_avegage_horsepower()
-analyzer.get_top_five_heaviest_cars()
-analyzer.get_cars_count_by_manifacturer()
-analyzer.get_cars_count_per_year()
-analyzer.save_data_to_csv('cars1.csv')
+# analyzer.load_data_from_json("cars.json")
+# analyzer.get_unique_car_count()
+# analyzer.get_avegage_horsepower()
+# analyzer.get_top_five_heaviest_cars()
+# analyzer.get_cars_count_by_manifacturer()
+# analyzer.get_cars_count_per_year()
+# analyzer.save_data_to_csv('cars1.csv')
