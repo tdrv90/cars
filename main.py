0e33914905eacdab61fe1665de0932c09af883af
Alexander Todorov <41835213+tdrv90@users.noreply.github.com>
2024-03-06 21:48:42 +0200

    Create main.py
    Define basic structure for the class CarAnalyzer

---
+++ main.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+
+
+class CarAnalyzer:
+    def __init__(self, filename='cars.json'):
+        self.filename = filename
+        self.data = None
+
+    def load_data(self):
+        # TODO:     Load the dataset from a JSON file.
+
+
+    def get_unique_car_count(self):
+        # TODO:     Print the number of unique cars in the dataset.
+
+
+    def get_avegage_horsepower(self):
+        # TODO:     Print the average horse power of all the cars.
+
+
+    def get_top_five_heaviest_cars(self):
+        # TODO:    Print the top 5 most heaviest cars.
+
+
+    def get_cars_count_by_manifacturer(self):
+        # TODO:     Print the number of cars made by each manufacturer.
+
+    
+    def get_cars_count_per_year(self):
+        # TODO:     Print the number of cars made each year.
+
+
+    def save_data_to_csv(self):
+        # TODO:     Save the dataset to a CSV file.
+
+
+
